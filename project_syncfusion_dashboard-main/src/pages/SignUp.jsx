9aac1913a1dd8caadd359978dd5a7343c368197a
AlexanderLukashuk <upiter465@gmail.com>
2024-04-13 19:49:12 +0500

    created pages for login and register

---
+++ project_syncfusion_dashboard-main/src/pages/SignUp.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+  const [userData, setUserData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8080/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData)
+      });
+      const data = await response.json();
+      console.log(data); // Вывести ответ от сервера на консоль
+      if (data.success) {
+        console.log('Registration successful', data);
+      } else {
+        console.log('Registration failed', data.message);
+      }
+    } catch (error) {
+      console.error('Error during registration:', error);
+    }
+  };
+  
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="username" placeholder="Username" value={userData.username} onChange={handleChange} required />
+        <input type="email" name="email" placeholder="Email" value={userData.email} onChange={handleChange} required />
+        <input type="password" name="password" placeholder="Password" value={userData.password} onChange={handleChange} required />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
