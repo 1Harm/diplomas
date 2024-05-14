011cd5303a533e0c78f33b3513c170a91cd8a5cf
AlexanderLukashuk <upiter465@gmail.com>
2024-05-15 01:30:17 +0500

    add mixed type for company data

--- server/mongodb/models/company.js
+++ server/mongodb/models/company.js
@@ -3,30 +3,7 @@ import mongoose from 'mongoose';
 const CompanySchema = new mongoose.Schema({
   name: { type: String, required: true },
   ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  industry: { type: String, required: false },
-  employees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  revenue: { type: Number, default: 0 }, // Total revenue of the company
-  customerAcquisitionCost: { type: Number, default: 0 }, // The cost of attracting a client
-  averageOrderValue: { type: Number, default: 0 }, // Average check
-  conversionRate: { type: Number, default: 0 }, // Conversion rate
-  churnRate: { type: Number, default: 0 }, // Customer churn rate
-  returnRate: { type: Number, default: 0 }, // Percentage of refunds
-  fulfillmentCosts: { type: Number, default: 0 }, // The cost of completing orders
-  customerSatisfactionScores: { type: Number, default: 0 }, // Customer satisfaction indicators
-  inventoryManagement: {
-    totalStock: { type: Number, default: 0 },
-    turnoverRate: { type: Number, default: 0 }, // Inventory turnover
-  },
-  marketingEfficiency: {
-    campaignROI: { type: Number, default: 0 }, // Effectiveness of marketing campaigns
-  },
-  customerLoyalty: {
-    repeatPurchaseRate: { type: Number, default: 0 }, // Frequency of repeat purchases
-  },
-  seasonalTrends: [{ 
-    month: String, 
-    sales: Number 
-  }], // Seasonality and sales trends
+  data: { type: mongoose.Schema.Types.Mixed }
 }, {
   timestamps: true
 });
