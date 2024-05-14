f5c545a3081f017d46620fdec7fde43ad64ad3d5
AlexanderLukashuk <upiter465@gmail.com>
2024-05-15 01:29:34 +0500

    add routes to work with files and graph

--- server/routes/company.routes.js
+++ server/routes/company.routes.js
@@ -1,13 +1,32 @@
 import express from 'express';
+import multer from 'multer';
 import CompanyController from '../controllers/company.controller.js';
 import authenticate from '../middleware/authenticate.js';
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'application/uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
+    }
+});
+
+// const upload = multer({ dest: 'application/uploads/' });
+const upload = multer({ storage: storage });
+
 router.post('/create', authenticate, CompanyController.createCompany);
 router.get('/', authenticate, CompanyController.getCompanyByUserId);
 router.get('/:id', authenticate, CompanyController.geCompany);
 router.put('/:id', authenticate, CompanyController.updateCompany);
+router.put('/:id/upload', authenticate, upload.single('file'), CompanyController.uploadCompanyData);
 router.delete('/:id', authenticate, CompanyController.deleteCompany);
+router.get('/:id/data', authenticate, CompanyController.getCompanyData);
+router.get('/:id/revenue', authenticate, CompanyController.getRevenueByMonth);
+router.get('/:id/revenue-by-month-and-year', authenticate, CompanyController.getRevenueByMonthAndYear);
+router.get('/user/:userId', authenticate, CompanyController.getCompaniesByUserId);
 
 export default router;
