4f0b02a3fbfacff8d3d21f7d8bc285b2971a31cf
AlexanderLukashuk <upiter465@gmail.com>
2024-05-15 01:32:15 +0500

    add methods to work with data and graph

--- server/controllers/company.controller.js
+++ server/controllers/company.controller.js
@@ -1,3 +1,4 @@
+import { json } from "express";
 import CompanyService from "../application/company.service.js";
 
 const CompanyController = {
@@ -33,6 +34,37 @@ const CompanyController = {
         }
     },
 
+    // ПЕРЕНЕСТИ ЛОГИКУ В СЕРВИС
+    async getCompanyData(req, res) {
+        try {
+            const companyId = req.params.id;
+            const company = await CompanyService.getCompanyById(companyId);
+            if (!company) {
+                return res.status(404).json({ message: 'Company not found' });
+            }
+            res.json(company.data);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    async uploadCompanyData(req, res) {
+        try {
+            const companyId = req.params.id;
+            const filePath = req.file.path;
+            
+            console.log(companyId);
+            console.log(filePath);
+            
+            const result = await CompanyService.uploadCSVData(companyId, req.file.filename);
+            
+            res.json({ message: result });
+        } catch (error) {
+            console.log("??????");
+            res.status(500).json({ message: error.message });
+        }
+    },
+
     async updateCompany(req, res) {
         try {
             const companyId = req.params.id;
@@ -54,6 +86,36 @@ const CompanyController = {
                 return res.status(404).json({ message: 'Company didnt find' });
             }
             res.json({ message: 'Company created successfully!' });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    async getRevenueByMonth(req, res) {
+        const companyId = req.params.id;
+        try {
+            const revenueData = await CompanyService.getRevenueByMonth(companyId);
+            res.json(revenueData);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    async getRevenueByMonthAndYear(req, res) {
+        const companyId = req.params.id;
+        try {
+            const revenueData = await CompanyService.getRevenueByMonthAndYear(companyId);
+            res.json(revenueData);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    async getCompaniesByUserId(req, res) {
+        try {
+            const userId = req.params.userId;
+            const companies = await CompanyService.getCompaniesByUserId(userId);
+            res.json(companies);
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
