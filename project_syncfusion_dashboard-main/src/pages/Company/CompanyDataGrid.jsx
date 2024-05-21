cc0afead71e3e9abab9201c21ac276e5cf874073
AlexanderLukashuk <upiter465@gmail.com>
2024-05-21 20:10:10 +0500

    add company name, button to upload new data and charts

--- project_syncfusion_dashboard-main/src/pages/Company/CompanyDataGrid.jsx
+++ project_syncfusion_dashboard-main/src/pages/Company/CompanyDataGrid.jsx
@@ -1,14 +1,18 @@
-import BoxHeader from "../../components/BoxHeader.tsx";
-import DashboardBox from "../../components/DashboardBox.tsx";
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { DataGrid } from '@mui/x-data-grid';
 import { useNavigate } from 'react-router-dom';
+import LineChart from '../../components/Charts/PredictionLineChart.jsx';
+import BarChart from '../../components/Charts/BarChart.jsx';
+import PieChart from '../../components/Charts/PieChart.jsx';
+import HistogramChart from '../../components/Charts/HistogramChart.jsx';
+import AreaChart from '../../components/Charts/AreaChart.jsx';
 
 const CompanyDataGrid = () => {
     const [companyData, setCompanyData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [companyName, setCompanyName] = useState('');
     const { companyId } = useParams();
     const navigate = useNavigate();
 
@@ -24,6 +28,7 @@ const CompanyDataGrid = () => {
                 });
 
                 if (response.data && typeof response.data.data === 'object') {
+                    setCompanyName(response.data.name);
                     const dataArray = Object.values(response.data.data);
                     const rows = dataArray[0].map((_, index) => {
                         const rowData = dataArray.map((column) => column[index]);
@@ -61,6 +66,26 @@ const CompanyDataGrid = () => {
         navigate(`/companies/${companyId}/upload`);
     };
 
+    const handleLineChartByMonthsPageRedirect = () => {
+        navigate(`/linechart-by-months`);
+    }
+    
+    const handleBarChartByMonthsPageRedirect = () => {
+        navigate(`/barchart-by-months`);
+    }
+    
+    const handlePieChartByMonthsPageRedirect = () => {
+        navigate(`/piechart-by-months`);
+    }
+    
+    const handleHistogramChartByMonthsPageRedirect = () => {
+        navigate(`/histogramchart-by-months`);
+    }
+    
+    const handleAreaChartByMonthsPageRedirect = () => {
+        navigate(`/areachart-by-months`);
+    }
+
     return (
         <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
             {loading ? (
@@ -75,29 +100,86 @@ const CompanyDataGrid = () => {
                             </button>
                         </div>
                     ) : (
-                        <div style={{ height: 400, width: '100%' }}>
-                            <DataGrid
-                                columnHeaderHeight={25}
-                                rowHeight={35}
-                                rows={companyData}
-                                columns={columns}
-                                sx={{
-                                    '& .MuiDataGrid-root': {
-                                        color: 'grey.300',
-                                        border: 'none',
-                                    },
-                                    '& .MuiDataGrid-cell': {
-                                        color: 'black',
-                                        borderBottom: '1px solid grey.800 !important',
-                                    },
-                                    '& .MuiDataGrid-columnHeaders': {
-                                        borderBottom: '1px solid grey.800 !important',
-                                    },
-                                    '& .MuiDataGrid-columnSeparator': {
-                                        visibility: 'hidden',
-                                    },
-                                }}
-                            />
+                        <div>
+                            <div className="flex justify-between items-center mb-4">
+                                <h1 className="text-2xl font-bold">{companyName}</h1>
+                                <button
+                                    className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
+                                    onClick={handleUploadPageRedirect}
+                                >
+                                    Upload new data
+                                </button>
+                            </div>
+
+                            <div style={{ height: 400, width: '100%' }}>
+                                <DataGrid
+                                    columnHeaderHeight={25}
+                                    rowHeight={35}
+                                    rows={companyData}
+                                    columns={columns}
+                                    sx={{
+                                        '& .MuiDataGrid-root': {
+                                            color: 'grey.300',
+                                            border: 'none',
+                                        },
+                                        '& .MuiDataGrid-cell': {
+                                            color: 'black',
+                                            borderBottom: '1px solid grey.800 !important',
+                                        },
+                                        '& .MuiDataGrid-columnHeaders': {
+                                            borderBottom: '1px solid grey.800 !important',
+                                        },
+                                        '& .MuiDataGrid-columnSeparator': {
+                                            visibility: 'hidden',
+                                        },
+                                    }}
+                                />
+                            </div>
+                            <div className="mt-8">
+                                <LineChart />
+                                <button
+                                    className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
+                                    onClick={handleLineChartByMonthsPageRedirect}
+                                >
+                                    By Months
+                                </button>
+                            </div>
+                            <div className="mt-8">
+                                <BarChart />
+                                <button
+                                    className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
+                                    onClick={handleBarChartByMonthsPageRedirect}
+                                >
+                                    By Months
+                                </button>
+                            </div>
+                            <div className="mt-8">
+                                <PieChart />
+                                <button
+                                    className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
+                                    onClick={handlePieChartByMonthsPageRedirect}
+                                >
+                                    By Months
+                                </button>
+                            </div>
+                            <div className="mt-8">
+                                <HistogramChart />
+                                <button
+                                    className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
+                                    onClick={handleHistogramChartByMonthsPageRedirect}
+                                >
+                                    By Months
+                                </button>
+                            </div>
+                            <div className="mt-8">
+                                <AreaChart />
+                                <button
+                                    className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none"
+                                    onClick={handleAreaChartByMonthsPageRedirect}
+                                >
+                                    By Months
+                                </button>
+                            </div>
                         </div>
                     )}
                 </>
